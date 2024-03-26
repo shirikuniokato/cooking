847977cfb11bd3f313c9f9a8ee613122540cf083
rikuto-nishioka <rikuto.nishioka@dena.com>
2024-03-27 03:42:38 +0900

    add api

---
+++ app/api/slack/route.ts
@@ -0,0 +1,32 @@
+import { App, Receiver } from "@slack/bolt";
+import { NextResponse } from "next/server";
+import { sql } from "@vercel/postgres";
+
+const app = new App({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: process.env.SLACK_BOT_TOKEN,
+});
+
+// Slackイベントのリスナーを登録
+app.command("/cook-add", async ({ command, ack, respond }) => {
+  await ack();
+  await respond("Hello world!");
+});
+
+app.message("hello", async ({ message, say }) => {
+  // イベントがトリガーされたチャンネルに say() でメッセージを送信します
+  await say(`Hello world!`);
+});
+
+export async function POST(request: Request) {
+  // リクエストハンドラを使ってBoltアプリを起動
+  try {
+    await app.start();
+    // await updateItem();
+    // const result =
+    //   await sql`SELECT item_id, item_type, content, order_id, user_id, false AS is_deleted FROM t_item t WHERE user_id = ORDER BY order_id ASC`;
+    return NextResponse.json({ status: "ok" });
+  } catch (e: any) {
+    return NextResponse.json({ status: "error", data: e.message });
+  }
+}
