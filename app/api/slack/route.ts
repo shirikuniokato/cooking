517a5736094656ea11170b04c240d303ba9d4b33
rikuto-nishioka <rikuto.nishioka@dena.com>
2024-03-27 04:15:01 +0900

    test

--- app/api/slack/route.ts
+++ app/api/slack/route.ts
@@ -1,11 +1,8 @@
-import { App, Receiver } from "@slack/bolt";
+import { App } from "@slack/bolt";
 import { NextResponse } from "next/server";
 import { sql } from "@vercel/postgres";
 
-const app = new App({
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-  token: process.env.SLACK_BOT_TOKEN,
-});
+const { appRunner } = require("./_app");
 
 // Slackイベントのリスナーを登録
 // app.command("/cook-add", async ({ command, ack, respond }) => {
@@ -18,21 +15,14 @@ const app = new App({
 //   await say(`Hello world!`);
 // });
 
-export async function POST(request: Request) {
+export async function POST(request: Request, response: Response) {
   // リクエストハンドラを使ってBoltアプリを起動
   try {
-    console.log(request);
+    await appRunner.handleEvents(request, response);
     // const json = await request.json();
     // await updateItem();
     // const result =
     //   await sql`SELECT item_id, item_type, content, order_id, user_id, false AS is_deleted FROM t_item t WHERE user_id = ORDER BY order_id ASC`;
-    return NextResponse.json({
-      status: "200",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      result: "verify",
-    });
   } catch (e: any) {
     return NextResponse.json({ status: "error", data: e.message });
   }
