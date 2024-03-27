39736f62af4a8f01940796aea5816dc7bbcfeb6e
rikuto-nishioka <rikuto.nishioka@dena.com>
2024-03-28 02:07:35 +0900

    条件分ｋいつ以下

--- app/item/[id]/page.tsx
+++ app/item/[id]/page.tsx
@@ -2,6 +2,11 @@ import Link from "next/link";
 import { headers } from "next/headers";
 import { config } from "@/app/lib/config";
 
+const formatYmd = (createDate: string): string => {
+  const date = new Date(createDate);
+  return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
+};
+
 async function getData(host: string, id: number) {
   const res = await fetch(`${config.apiPrefix}${host}/api/item/${id}`);
 
@@ -23,7 +28,7 @@ export default async function Page({
   return (
     <>
       <section id="ancr_ticket" className="tick_sec c-anim-up move mt-[100px]">
-        <div className="container2">
+        <div className="container2 custom2:custom-goods-inner-div">
           <h3>DETAILS</h3>
           <p className="txt_price">{data.name}</p>
           <ul>
@@ -31,14 +36,22 @@ export default async function Page({
               <dl>
                 <dt>{data.is_cook ? "作成済み" : "未作成"}</dt>
                 <dd className="soldout">
-                  <p className="details">■登録日：{data.created_at}</p>
+                  <p className="details">
+                    ■登録日：{formatYmd(data.created_at)}
+                  </p>
                   <p className="details">
                     ■参考サイト：
-                    <Link className="link" href={data.link}>
-                      こちら
-                    </Link>
+                    {data.link ? (
+                      <Link className="link" href={data.link}>
+                        こちら
+                      </Link>
+                    ) : (
+                      "無し"
+                    )}
                   </p>
-                  <p className="details">■メモ：{data.memo}</p>
+                  <p className="details">
+                    ■メモ：{data.memo ? data.memo : "無し"}
+                  </p>
                 </dd>
               </dl>
             </li>
